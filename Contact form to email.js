1a323065617bb45153b05f909d57836d2f572b94
zkanri <anrizakarashvili@gmail.com>
2024-10-30 12:51:14 -0700

    Add files via upload

---
+++ Contact form to email.js
@@ -0,0 +1,28 @@
+function emailSend(){
+
+	var userName = document.getElementById('name').value;
+	var phone = document.getElementById('phone').value;
+	var email = document.getElementById('email').value;
+
+	var messageBody = "Name " + userName +
+	"<br/> Phone " + phone +
+	"<br/> Email " + email;
+	Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "host@gmail.com",
+    Password : "Password",
+    To : 'reviever@gmail.com',
+    From : "website@gmail.com",
+    Subject : "This is the subject",
+    Body : messageBody
+}).then(
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
+);
+}
